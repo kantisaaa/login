550fa425eee76e9f29fa98ba14302545cbc88c30
kantisaaa <144190765+kantisaaa@users.noreply.github.com>
2024-07-30 19:00:39 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>งานตรวจสอบสิทธิ์</title>
+    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/Login.css">
+</head>
+<body>
+    <div class="main">
+        <div class="Left">
+            <p class="htxt">ยินดีต้อนรับ</p>
+            <p class="txt1">ชื่อเข้าใช้ระบบ/e-mail</p>
+            <input type="text"class="itxt" fdprocessedid="eejjhk">
+            <p class="txt1">รหัสผ่าน</p>
+            <input type="text"class="itxt" fdprocessedid="m66was">
+            <p class="ichk">
+                <input type="checkbox" name id>
+                "ลืมรหัสผ่าน ?"
+            </p>
+            <button type="submit" fdprocessedid="fnzaqf">Login</button>
+            <p class="txt2">ต้องการสมัครสมาชิกใหม่,คลิก!</p>                 
+        </div>
+        <div class="Right">
+            <img src="img/main1.jpg"alt>
+        </div>
+    </div>
+</body>
+</html>
